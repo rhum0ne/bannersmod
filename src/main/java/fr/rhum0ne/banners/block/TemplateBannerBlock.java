806d5684d91401870652224b2b5289eb5d3a4e44
Romain <43304807+rhum0ne@users.noreply.github.com>
2025-08-04 22:07:26 +0200

    Fix banner textures and state

--- src/main/java/fr/rhum0ne/banners/block/TemplateBannerBlock.java
+++ src/main/java/fr/rhum0ne/banners/block/TemplateBannerBlock.java
@@ -1,7 +1,11 @@
 package fr.rhum0ne.banners.block;
 
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 
 /**
  * Simple placeholder banner block.
@@ -10,5 +14,11 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 public class TemplateBannerBlock extends HorizontalDirectionalBlock {
     public TemplateBannerBlock() {
         super(BlockBehaviour.Properties.of().strength(1.0F).noOcclusion());
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 }
