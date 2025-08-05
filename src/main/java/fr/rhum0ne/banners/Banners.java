c2c42d40c9f8028c677ee6b3787254ef82ab2f98
Romain <43304807+rhum0ne@users.noreply.github.com>
2025-08-05 15:40:10 +0200

    banners patterns not working

--- src/main/java/fr/rhum0ne/banners/Banners.java
+++ src/main/java/fr/rhum0ne/banners/Banners.java
@@ -1,9 +1,14 @@
 package fr.rhum0ne.banners;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.BannerPatternItem;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BannerPattern;
+import net.minecraft.core.registries.Registries;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -23,6 +28,8 @@ public class Banners {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     // Create a Deferred Register to hold Items which will all be registered under the "banners" namespace
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    // Create a Deferred Register to hold Banner Patterns which will all be registered under the "banners" namespace
+    public static final DeferredRegister<BannerPattern> BANNER_PATTERNS = DeferredRegister.create(Registries.BANNER_PATTERN, MODID);
     // Register a simple banner block and its item as an example template
     public static final RegistryObject<Block> TEMPLATE_BANNER = BLOCKS.register("template_banner", () -> new TemplateBannerBlock());
     public static final RegistryObject<Item> TEMPLATE_BANNER_ITEM = ITEMS.register("template_banner", () -> new BlockItem(TEMPLATE_BANNER.get(), new Item.Properties()));
@@ -30,6 +37,27 @@ public class Banners {
     public static final RegistryObject<Item> LANNISTER_BANNER_ITEM = ITEMS.register("lannister_banner", () -> new BlockItem(LANNISTER_BANNER.get(), new Item.Properties()));
     public static final RegistryObject<Block> MAISON_TULLY_BANNER = BLOCKS.register("maison_tully_banner", () -> new TemplateBannerBlock());
     public static final RegistryObject<Item> MAISON_TULLY_BANNER_ITEM = ITEMS.register("maison_tully_banner", () -> new BlockItem(MAISON_TULLY_BANNER.get(), new Item.Properties()));
+    public static final RegistryObject<BannerPattern> LANNISTER_PATTERN =
+            BANNER_PATTERNS.register("lannister",
+                    () -> new BannerPattern(Banners.MODID + ":lannister"));
+
+    public static final RegistryObject<BannerPattern> MAISON_TULLY_PATTERN =
+            BANNER_PATTERNS.register("maison_tully",
+                    () -> new BannerPattern(Banners.MODID + ":maison_tully"));
+    public static final TagKey<BannerPattern> LANNISTER_PATTERN_ITEM_TAG =
+            TagKey.create(Registries.BANNER_PATTERN, new ResourceLocation(MODID, "pattern_item/lannister"));
+
+    public static final TagKey<BannerPattern> MAISON_TULLY_PATTERN_ITEM_TAG =
+            TagKey.create(Registries.BANNER_PATTERN, new ResourceLocation(MODID, "pattern_item/maison_tully"));
+
+    public static final RegistryObject<Item> LANNISTER_BANNER_PATTERN_ITEM =
+            ITEMS.register("lannister_banner_pattern",
+                    () -> new BannerPatternItem(LANNISTER_PATTERN_ITEM_TAG, new Item.Properties().stacksTo(1)));
+
+    public static final RegistryObject<Item> MAISON_TULLY_BANNER_PATTERN_ITEM =
+            ITEMS.register("maison_tully_banner_pattern",
+                    () -> new BannerPatternItem(MAISON_TULLY_PATTERN_ITEM_TAG, new Item.Properties().stacksTo(1)));
+
 
     public Banners() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -38,6 +66,8 @@ public class Banners {
         BLOCKS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so items get registered
         ITEMS.register(modEventBus);
+        // Register banner patterns
+        BANNER_PATTERNS.register(modEventBus);
         // Add items to existing creative tabs
         modEventBus.addListener(this::addCreative);
     }
@@ -47,6 +77,10 @@ public class Banners {
             event.accept(TEMPLATE_BANNER_ITEM);
             event.accept(LANNISTER_BANNER_ITEM);
             event.accept(MAISON_TULLY_BANNER_ITEM);
+        }
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(LANNISTER_BANNER_PATTERN_ITEM);
+            event.accept(MAISON_TULLY_BANNER_PATTERN_ITEM);
         }
     }
 }
